01898aa59e42defadb2995027642fdbb091fcad3
zaeyon <wodus5677@gmail.com>
2023-08-09 19:44:21 +0900

    fix: youtube category update

--- app/components/CampaignForm/BasicForm.tsx
+++ app/components/CampaignForm/BasicForm.tsx
@@ -1,56 +1,61 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
-import InfoInput from '../InfoInput';
-import FileInput from '../FileInput';
+import InfoInput from "../InfoInput";
+import FileInput from "../FileInput";
 
 const Container = styled.div`
-    margin-top: 24px;
+  margin-top: 24px;
 `;
 
 const SectionTitle = styled.div`
-    font-family: 'Pretendard';
-    font-size: 17px;
-    font-weight: 600;
-    line-height: 27px;
-    letter-spacing: -0.015em;
-    text-align: left;
-`
+  font-family: "Pretendard";
+  font-size: 17px;
+  font-weight: 600;
+  line-height: 27px;
+  letter-spacing: -0.015em;
+  text-align: left;
+`;
 
 interface props {
-    brandName: string;
-    title: string;
-    thumbnailImageFile: any;
-    changeThumbnailImage: (file: any, src: any) => void;
-    changeBrandName: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    changeTitle: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  brandName: string;
+  title: string;
+  thumbnailImageFile: any;
+  changeThumbnailImage: (file: any, src: any) => void;
+  changeBrandName: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  changeTitle: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function BasicForm({brandName, title, thumbnailImageFile, changeThumbnailImage, changeBrandName, changeTitle} : props) {
-    return (
-        <Container>
-            <SectionTitle>
-                Basics
-            </SectionTitle>
-            <InfoInput
-            onChangeInput={changeBrandName}
-            value={brandName}
-            label={"Brand Name"}
-            placeholder={"Enter your brand name"}
-            />
-            <InfoInput
-            onChangeInput={changeTitle}
-            value={title}
-            label={"Campaign name"}
-            placeholder={"Enter your campaign name"}
-            />
-            <FileInput
-            value={thumbnailImageFile}
-            changeFile={changeThumbnailImage}
-            label={"Thumbnail Image"}
-            placeholder={"Select image files from your device"}
-            description={"Only png, JPEG files are accepted"}
-            accept={"image/*"}
-            />
-        </Container>
-    )
+export default function BasicForm({
+  brandName,
+  title,
+  thumbnailImageFile,
+  changeThumbnailImage,
+  changeBrandName,
+  changeTitle,
+}: props) {
+  return (
+    <Container>
+      <SectionTitle>Basics</SectionTitle>
+      <InfoInput
+        onChangeInput={changeBrandName}
+        value={brandName}
+        label={"Brand Name"}
+        placeholder={"Enter your brand name"}
+      />
+      <InfoInput
+        onChangeInput={changeTitle}
+        value={title}
+        label={"Campaign name"}
+        placeholder={"Enter your campaign name"}
+      />
+      <FileInput
+        value={thumbnailImageFile}
+        changeFile={changeThumbnailImage}
+        label={"Thumbnail Image"}
+        placeholder={"Select image files from your device"}
+        description={"Only JPEG files are accepted"}
+        accept={"image/*"}
+      />
+    </Container>
+  );
 }
